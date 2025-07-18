776682f8a070ab53b7553ba15fb50d7edb6f1be2
Neeraj Yadav <nrjydv1997@gmail.com>
2025-07-18 12:30:50 +0530

    Update docker_build.groovy

--- vars/docker_build.groovy
+++ vars/docker_build.groovy
@@ -1,5 +1,4 @@
 def call(String projectName,String imageTag,String dockerHubUser){
-  sh "docker rm -f $(docker ps -aq)"
   sh "docker rmi ${dockerHubUser}/${projectName}:${imageTag}"
   sh "docker build -t ${dockerHubUser}/${projectName}:${imageTag} ."
 }
