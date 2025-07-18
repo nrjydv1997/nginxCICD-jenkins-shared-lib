a4f9fd7afa752bbbce7f910608f21745be1d8f94
Neeraj Yadav <nrjydv1997@gmail.com>
2025-07-18 12:09:45 +0530

    Update docker_push.groovy

--- vars/docker_push.groovy
+++ vars/docker_push.groovy
@@ -1,7 +1,6 @@
 def call(String imageTag){
   withCredentials([usernamePassword(credentialsId: 'dockerCred', passwordVariable: 'dockerPass', usernameVariable: 'dockerUser')]) {
                   sh "docker login -u ${dockerUser} -p ${dockerPass}"
-                  sh "docker image tag nginx-app:latest ${env.dockerUser}/nginx-app:latest"
                   sh "docker push ${env.dockerUser}/nginx-app:latest"
                 }
 }
